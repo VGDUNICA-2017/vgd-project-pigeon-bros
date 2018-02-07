66c61249e1030467d2f6a8de577dc88d59de6dad
Giacomo Medda <jackm.medda@gmail.com>
2018-02-07 02:14:17 +0100

    Level 2 Done and Boss 2 work
    Completed the Level 2 environment, added rain, about to be completed Level for 2nd Boss.

---
+++ Assets/Scripts/RainCamera.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RainCamera : MonoBehaviour {
+
+	float offsetX, offsetY;
+	Vector3 newPos, posCam;
+
+	// Use this for initialization
+	void Start () {
+		offsetX = Mathf.Abs(Camera.main.transform.position.x - transform.position.x);
+		offsetY = Mathf.Abs(Camera.main.transform.position.y - transform.position.y);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		posCam = Camera.main.transform.position;
+		newPos = new Vector3 (posCam.x + offsetX, posCam.y + offsetY, 0);
+		transform.position = newPos;
+	}
+}
